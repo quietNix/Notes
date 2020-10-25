b8beec9c6e5b1782d4cf2be0a52187a83e09bd3f
quietNix <42882230+quietNix@users.noreply.github.com>
2020-10-26 00:05:20 +0530

    Add google OAuth 2.0 + true session use. 1. storing eml-psswrd auth and google oauth in different db collections    namely posts and postGoogles. 2. using _id/ googleID stored in session for finding particular user account. TODO 2: email verification in eml-psswrd auth.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const { text } = require("body-parser");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose"); //passport-local will be required by this
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const homeStartingContent = "hello bsbcd nbhscdz ac hkc  hb  cssdbchb cewmbebdkbkcufiabkvfdld fjd,";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis et massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -19,7 +20,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(session({
-  secret: "Thisisasecret",
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false
 }));
@@ -29,7 +30,7 @@ app.use(passport.session());
 
 
 // mongoose.connect('mongodb://localhost:27017/blogWebsiteDB', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect(process.env.MONGOCONNECTION, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useFindAndModify', false);    //for findOneAndUpdate()
 mongoose.set('useCreateIndex', true);
 
@@ -39,7 +40,6 @@ const itemsSchema = ({
 });
 
 const postsArray = new mongoose.Schema({
-  // name: String,
   posts: [itemsSchema]
 });
 
@@ -47,26 +47,81 @@ postsArray.plugin(passportLocalMongoose);
 
 const Post = mongoose.model("post", postsArray);
 
-passport.use(Post.createStrategy());
-passport.serializeUser(Post.serializeUser());
-passport.deserializeUser(Post.deserializeUser());
+const postsArrayGoogle = new mongoose.Schema({
+  googleID: String,
+  username: String,
+  posts: [itemsSchema]
+});
 
-app.get("/", function (req, res) {
-  if (req.isAuthenticated()) {
-    console.log(req.user.username);
-    res.redirect("/" + req.user.username);
-  } else {
-    res.render("LoginRegister");
+const PostGoogle = mongoose.model("postGoogle", postsArrayGoogle);
+
+passport.use(Post.createStrategy());
+
+passport.serializeUser(function (user, done) {
+  done(null, { _id: user._id, googleID: user.googleID });
+});
+
+passport.deserializeUser(function (id, done) {
+  // console.log("user object stored in session: ");    //debug 
+  // console.log(id);
+  if (id._id && !id.googleID) {
+    Post.findOne({ _id: id._id }, function (err, user) {
+      done(err, user);
+    });
+  } else if (id.googleID && id.googleID) {
+    PostGoogle.findOne({ googleID: id.googleID }, function (err, user) {
+      done(err, { _id: user._id, googleID: user.googleID });
+    });
   }
 });
 
+//google oAuth 20 strategy
+passport.use(new GoogleStrategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: process.env.GOOGLE_OAUTH_CALLBACK
+},
+  function (accessToken, refreshToken, profile, cb) {
+    PostGoogle.findOne({ googleID: profile.id }, function (err, post) {
+      if (err) {
+        res.redirect("/auth/register");
+      } else {
+        if (post) {
+          return cb(err, post);
 
-///authentication
-app.get("/auth/register", function (req, res) {
+        } else {
+
+          const newItem = ({
+            title: profile._json.email,
+            content: homeStartingContent
+          });
+
+          postGoogle = new PostGoogle({
+            googleID: profile.id,
+            username: profile._json.email,
+            posts: [newItem]
+          });
+
+          postGoogle.save({}, function (err, result) {
+            if (err) {
+              res.redirect("/auth/register");
+            } else {
+              return cb(err, result);
+            }
+          });
+        }
+      }
+    });
+  }
+));
+
+
+///authentication via simple username and password
+app.get("/register", function (req, res) {
   res.render("register");
 });
 
-app.get("/auth/login", function (req, res) {
+app.get("/login", function (req, res) {
   res.render("login");
 });
 
@@ -83,10 +138,8 @@ app.post("/register", function (req, res) {
       res.redirect("/auth/register");
     }
     else {
-      console.log(", NotExist.");
       passport.authenticate('local')(req, res, function () {      //creating cookie in browser telling this user already authenticated
-        console.log("authenticated");
-        res.redirect("/" + req.body.username);
+        res.redirect("/");
         return;
       });
     }
@@ -102,121 +155,166 @@ app.post("/login", function (req, res) {
     if (err) console.log("err: " + err);
     else {
       passport.authenticate('local')(req, res, function () {
-        res.redirect("/" + req.user.username);
+        res.redirect("/");
       });
     }
   });
 });
 
-app.get("/:customRoute", function (req, res) {
-  if (req.isAuthenticated()) {
-    console.log(req.user.username);
-    const customListName = req.params.customRoute;
-    if (customListName.toLowerCase() == "favicon.ico") return;
-    console.log(customListName);
-    if (customListName != req.user.username) {
-      res.redirect("/" + req.user.username);
-      return;
-    }
-    if (customListName == "compose") {
-      res.redirect("/compose/home");
-      return;
-    }
-    Post.findOne({ username: customListName }, function (err, foundList) {
-      if (err) console.log("err" + err);
-      else {
-        console.log(", Exists.");
-        res.render("home", { posts: foundList.posts, user: customListName });
-      }
-    });
-  } else {
-    res.redirect("/auth/login");
-  }
+//Authenticating via google oauth 20
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] }) //popup for signing in google
+);
+
+app.get('/auth/google/notes', passport.authenticate('google', { failureRedirect: "/login" }), function (req, res) {
+  res.redirect("/");
 });
 
-app.get("/posts/:user/:postID", function (req, res) {
+
+app.get("/", function (req, res) {
   if (req.isAuthenticated()) {
-    console.log(req.user.username);
-    if (req.params.user != req.user.username) {
-      res.redirect("/" + req.user.username);
-      return;
-    }
-    const postAuthor = req.params.user;
-    Post.findOne({ username: postAuthor }, function (err, foundList) {
-      if (err) console.log(err);
-      else {
-        if (!foundList) console.log("list not found");
+    // console.log("/");                      //debug
+    // console.log("req.user");
+    // console.log(req.user);
+    // console.log("req.session.passport.user");
+    // console.log(req.session.passport.user);
+    //TODO: difference between req.session.passport.user and req.user
+    //      and which to use when.
+    if (req.session.passport.user._id && !req.session.passport.user.googleID) {
+      Post.findOne({ _id: req.session.passport.user._id }, function (err, foundList) {
+        if (err) console.log("err" + err);
         else {
-          foundList.posts.forEach(function (post) {
-            if (post._id == req.params.postID) {
-              res.render("post", { user: postAuthor, authorsPost: post });
-            }
-          });
+          res.render("home", { posts: foundList.posts, user: "customListName" });
         }
-      }
-    });
+      });
+    } else if (req.session.passport.user._id && req.session.passport.user.googleID) {
+      PostGoogle.findOne({ googleID: req.session.passport.user.googleID }, function (err, foundList) {
+        if (err) console.log("err" + err);
+        else {
+          if (!foundList) console.log("no user exists" + foundList);
+          else {
+            res.render("home", { posts: foundList.posts, user: req.session.passport.user.username });
+          }
+        }
+      });
+    } else res.redirect("/");
   } else {
     res.render("LoginRegister");
   }
 });
 
-app.get("/compose/:user", function (req, res) {
+app.get("/posts/:postID", function (req, res) {
   if (req.isAuthenticated()) {
-    console.log(req.user.username);
-    authUser = req.user.username;
-    if (req.params.user == authUser) {
-      res.render("compose", { user: authUser });
-    } else {
-      res.redirect("/compose/" + authUser);
-      return;
+    if (req.session.passport.user._id && !req.session.passport.user.googleID) {
+      Post.findOne({ _id: req.session.passport.user._id }, function (err, foundList) {
+        if (err) console.log(err);
+        else {
+          if (!foundList) console.log("list not found");
+          else {
+            foundList.posts.forEach(function (post) {
+              if (post._id == req.params.postID) {
+                res.render("post", { authorsPost: post });
+              }
+            });
+          }
+        }
+      });
+    } else if (req.session.passport.user._id && req.session.passport.user.googleID) {
+      PostGoogle.findOne({ googleID: req.session.passport.user.googleID }, function (err, foundList) {
+        if (err) console.log(err);
+        else {
+          if (!foundList) console.log("list not found");
+          else {
+            foundList.posts.forEach(function (post) {
+              if (post._id == req.params.postID) {
+                res.render("post", { authorsPost: post });
+              }
+            });
+          }
+        }
+      });
     }
   } else {
     res.render("LoginRegister");
   }
 });
 
-app.post("/compose/:user", function (req, res) {
+app.get("/compose", function (req, res) {
   if (req.isAuthenticated()) {
-    console.log(req.user.username);
-    if (req.params.user != req.user.username) {
-      res.redirect("/" + req.user.username);
-      return;
-    }
-    const customListName = req.params.user;
+    res.render("compose");
+  } else {
+    res.render("LoginRegister");
+  }
+});
+
+app.post("/compose", function (req, res) {
+  if (req.isAuthenticated()) {
     const newItem = ({
       title: req.body.postTitle,
       content: req.body.postBody
     });
-    Post.findOne({ username: customListName }, function (err, foundList) {
-      if (err) console.log("fatal error asking element from list which doesnt exist.");
-      else {
-        if (foundList) {
-          foundList.posts.push(newItem);
-          foundList.save({}, function (err, result) {
-            if (err) console.log("err" + err);
-            else {
-              res.redirect("/" + customListName);
-            }
-          });
-        } else {
-          res.redirect("/" + customListName);
+
+    if (req.session.passport.user._id && !req.session.passport.user.googleID) {
+      Post.findOne({ _id: req.session.passport.user._id }, function (err, foundList) {
+        if (err) console.log("fatal error asking element from list which doesnt exist.");
+        else {
+          if (foundList) {
+            foundList.posts.push(newItem);
+            foundList.save({}, function (err, result) {
+              if (err) console.log("err" + err);
+              else {
+                res.redirect("/");
+              }
+            });
+          } else {
+            res.redirect("/");
+          }
         }
-      }
-    });
+      });
+    } else if (req.session.passport.user._id && req.session.passport.user.googleID) {
+      PostGoogle.findOne({ googleID: req.session.passport.user.googleID }, function (err, foundList) {
+        if (err) console.log("fatal error asking element from list which doesnt exist.");
+        else {
+          if (foundList) {
+            foundList.posts.push(newItem);
+            foundList.save({}, function (err, result) {
+              if (err) console.log("err" + err);
+              else {
+                res.redirect("/");
+              }
+            });
+          } else {
+            res.redirect("/");
+          }
+        }
+      });
+    }
   } else {
     res.render("LoginRegister");
   }
 });
 
 app.post("/delete", function (req, res) {
-  const customListName = req.body.user;
-  const checkedPostId = req.body.postID;
-  Post.findOneAndUpdate({ username: customListName }, { $pull: { posts: { _id: checkedPostId } } }, function (err) {
-    if (err) console.log(err);
-    else {
-      res.redirect("/" + customListName);
+  if (req.isAuthenticated()) {
+    const checkedPostId = req.body.postID;
+    if (req.session.passport.user._id && !req.session.passport.user.googleID) {
+      Post.findOneAndUpdate({ _id: req.session.passport.user._id }, { $pull: { posts: { _id: checkedPostId } } }, function (err) {
+        if (err) console.log(err);
+        else {
+          res.redirect("/");
+        }
+      });
+    } else if (req.session.passport.user._id && req.session.passport.user.googleID) {
+      PostGoogle.findOneAndUpdate({ googleID: req.session.passport.user.googleID }, { $pull: { posts: { _id: checkedPostId } } }, function (err) {
+        if (err) console.log(err);
+        else {
+          res.redirect("/");
+        }
+      });
     }
-  });
+  } else {
+    res.render("LoginRegister");
+  }
 });
 
 app.get("/cc/about", function (req, res) {
@@ -224,7 +322,7 @@ app.get("/cc/about", function (req, res) {
 });
 
 app.get("/cc/contact", function (req, res) {
-  res.render("contact", { text: contactContent });
+  res.render("contact");
 });
 
 app.get("/cc/logout", function (req, res) {
